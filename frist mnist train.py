5c154bb0d384d0a3525ed96e25e6e2eba294570a
olker klelke <57739576+olker-klelk@users.noreply.github.com>
2020-08-11 00:11:51 +0800

    update

--- frist mnist train.py
+++ frist mnist train.py
@@ -2,15 +2,43 @@ import numpy as np
 import sys, os
 sys.path.append(os.pardir)
 from dataset.mnist import load_mnist
-from PIL import Image
+import pickle
 
-def img_show(img):
-    pil_img=Image.fromarray(np.uint8(img))
-    pil_img.show()
-(x_train,t_train),(x_test,t_test)=load_mnist(flatten=True,normalize=False)
-img=x_train[0]
-label=t_train[0]
-print(label)
-img=img.reshape(28,28)
-print(img.shape)
-img_show(img)
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def softmax(x):
+    c=np.max(x,axis=-1, keepdims=True)
+    x=x-c
+    return np.exp(x)/np.sum(np.exp(x),axis=-1, keepdims=True)
+
+def get_data():
+    (x_train,t_train),(x_test,t_test)=load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
+def init_network():
+    with open("sample_weight.pkl","rb") as f:
+        network=pickle.load(f)
+    return network
+
+def predict(network,x):
+    W1,W2,W3=network['W1'],network['W2'],network['W3']
+    b1,b2,b3=network['b1'],network['b2'],network['b3']
+    a1=np.dot(x,W1)+b1
+    z1=sigmoid(a1)
+    a2=np.dot(z1,W2)+b2
+    z2=sigmoid(a2)
+    a3=np.dot(z2,W3)+b3
+    y=softmax(a3)
+    return y
+
+x, t = get_data()
+network = init_network()
+accuracy_cnt=0
+for i in range(0,len(x),100):
+    y=predict(network, x[i:i+100])
+    p=np.argmax(y,axis=1)
+    accuracy_cnt+=np.sum(p==t[i:i+100])
+        
+print(len(x))
+print("Acc:"+str(float(accuracy_cnt)/len(x)))
