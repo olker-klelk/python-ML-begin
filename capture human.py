ff29d4c0e2394e1daded06a89a4d328d98170e00
olker klelke <57739576+olker-klelk@users.noreply.github.com>
2021-03-01 22:20:10 +0800

    update

---
+++ capture human.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+
+np.sum(12)
+img = cv2.imread(r'./image/image01.jpg',cv2.IMREAD_GRAYSCALE)
+
+print(type(img))
+print(img.shape)
+cv2.imshow('imgg',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
